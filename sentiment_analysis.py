da27b69a0d61c7e5d12c4c3ceb31e5204790dd67
vasRK <srinivas.r553@gmail.com>
2017-09-23 14:36:30 +0530

    Separate result for twitter & News

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -16,6 +16,11 @@ class TwitterClient(object):
     neutral_news = []
     all_news = []
 
+    positive_tweets = []
+    negative_tweets = []
+    neutral_tweets = []
+    all_tweets = []
+
     def __init__(self):
 
         # attempt authentication
@@ -37,7 +42,7 @@ class TwitterClient(object):
                 # ignore retweets
                 if (not tweet.retweeted) and ('RT @' not in tweet.text):
                     # print(tweet.text)
-                    self.all_news.append(self.clean_text(tweet.text))
+                    self.all_tweets.append(self.clean_text(tweet.text))
         except Exception as e:
             # print error (if any)
             print("Error : " + str(e))
@@ -58,6 +63,9 @@ class TwitterClient(object):
 
     def predict_stock_sentiment(self, quote, count=100):
         result = {}
+        news_sentiment = {}
+        tweet_sentiment = {}
+
         # read twitter feed, use keywords with company quotes + " Stocks " etc.
         tokens = quote.split(":")
         if len(tokens) > 1:
@@ -80,18 +88,18 @@ class TwitterClient(object):
                 self.negative_news.append(news)
 
         total_news_count = len(self.all_news)
-        positive_count = len(self.positive_news)
-        negative_count = len(self.negative_news)
-        neutral_count = len(self.neutral_news)
+        positive_news_count = len(self.positive_news)
+        negative_news_count = len(self.negative_news)
+        neutral_news_count = len(self.neutral_news)
 
-        result["positive"] = (100 * positive_count / total_news_count)
-        result["negative"] = (100 * negative_count / total_news_count)
-        result["neutral"] = (100 * neutral_count / total_news_count)
+        news_sentiment['positive_per'] = (100 * (positive_news_count / total_news_count))
+        news_sentiment["negative_per"] = (100 * (negative_news_count / total_news_count))
+        news_sentiment["neutral_per"] = (100 * (neutral_news_count / total_news_count))
 
-        result["positive_count"] = positive_count
-        result["negative_count"] = negative_count
-        result["neutral_count"] = neutral_count
-
+        news_sentiment["positive_count"] = positive_news_count
+        news_sentiment["negative_count"] = negative_news_count
+        news_sentiment["neutral_count"] = neutral_news_count
+                      
         print("\n\nNegative news:")
         for news in self.negative_news[:10]:
             print('-ve news- ' + news)
@@ -104,12 +112,52 @@ class TwitterClient(object):
         for news in self.neutral_news[:10]:
             print('= news- ' + news)
 
+        for tweet in self.all_tweets:
+            # saving sentiment of tweet
+            # print('News- '+news)
+            analysis = TextBlob(tweet)
+            if analysis.sentiment.polarity > 0:
+                self.positive_tweets.append(tweet)
+            elif analysis.sentiment.polarity == 0:
+                self.neutral_tweets.append(tweet)
+            else:
+                self.negative_tweets.append(tweet)
+
+        total_tweet_count = len(self.all_tweets)
+        positive_tweet_count = len(self.positive_tweets)
+        negative_tweet_count = len(self.negative_tweets)
+        neutral_tweet_count = len(self.neutral_tweets)
+
+        tweet_sentiment["positive_per"] = (100 * (positive_tweet_count / total_tweet_count))
+        tweet_sentiment["negative_per"] = (100 * (negative_tweet_count / total_tweet_count))
+        tweet_sentiment["neutral_per"] = (100 * (neutral_tweet_count / total_tweet_count))
+
+        tweet_sentiment["positive_count"] = positive_tweet_count
+        tweet_sentiment["negative_count"] = negative_tweet_count
+        tweet_sentiment["neutral_count"] = neutral_tweet_count
+
+        print("\n\nNegative tweets:")
+        for tweet in self.negative_tweets[:10]:
+            print('-ve tweet- ' + tweet)
+
+        print("\n\nPositive tweet:")
+        for tweet in self.positive_tweets[:10]:
+            print('+ve tweet- ' + tweet)
+
+        print("\n\nNeutral tweet:")
+        for tweet in self.neutral_tweets[:10]:
+            print('= tweet- ' + tweet)
+
+        result["tweet_sentiment"] = tweet_sentiment
+        result["news_sentiment"] = news_sentiment
+
         return result
+
 
 def main():
     client = TwitterClient()
     # client.get_google_finance_news_feed('NASDAQ:FB',100)
-    result = client.predict_stock_sentiment(quote='NASDAQ:AAPL', count=500)
+    result = client.predict_stock_sentiment(quote='NSE:TCS', count=500)
     print(result)
 
 
